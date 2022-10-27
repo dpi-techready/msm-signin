41a16bfe614415a67a1ac66d912b53a1ce6ec02d
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-10-27 02:35:22 +0000

    Able to bookmark and show buttons conditionally

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
     b.movie_id = params.fetch("the_movie_id")
     b.save
 
-    redirect_to("/")
+    redirect_to("/bookmarks", :notice => "Bookmarked movie!")
   end  
 
   def index
